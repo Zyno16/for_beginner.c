82b9b6254d0f37f2dae1e73d28363169c21f5a01
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-08-13 15:50:57 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int n,i;
+
+    printf("enter a number");
+    scanf("%d",&n);
+    for( i=n+1 ; i<=n+10 ; i++ )
+    printf("%d\n",i);
+
+
+
+
+    return 0;
+}
